4ee17f826b257395f03d286863ed7a19e95acaa2
Leon Si <contact@leonzalion.com>
2022-03-18 12:10:41 -0400

    feat: saving cookies

--- packages/desktop-app/test/bin/admin-console.ts
+++ packages/desktop-app/test/bin/admin-console.ts
@@ -5,40 +5,48 @@ import puppeteer from 'puppeteer';
 import $ from 'jquery';
 import pWaitFor from 'p-wait-for';
 
-const __require = createRequire(import.meta.url);
+const require = createRequire(import.meta.url);
 
 const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
+await page.setBypassCSP(true);
+
+// Load cookies if available
+if (fs.existsSync('cookies.json')) {
+	const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8')) as any[];
+	await page.setCookie(...cookies);
+}
 
 console.info('Navigating to Sign in page...');
 await page.goto('https://accounts.google.com/signin/v2');
 
 console.info('Waiting for myaccount.google.com...');
-await pWaitFor(() => {
-	console.log(page.url());
-	return page.url().startsWith('https://myaccount.google.com');
-});
+await pWaitFor(() => page.url().startsWith('https://myaccount.google.com'));
+
+const cookies = await page.cookies();
+const cookiesJson = JSON.stringify(cookies);
+
+fs.writeFileSync('cookies.json', cookiesJson);
 
 console.info('Navigating to admin console...');
 await page.goto('https://admin.google.com/ac/apps/gmail/routing');
 
 console.info('Waiting for admin.google.com...');
-await pWaitFor(() => {
-	console.log(page.url());
-	return page.url().startsWith('https://admin.google.com');
-});
+await pWaitFor(() => page.url().startsWith('https://admin.google.com'));
 
 console.info('Loading jQuery onto the page...');
 await page.evaluate(
-	fs.readFileSync(path.join(__require.resolve('jquery')), 'utf8')
+	fs.readFileSync(path.join(require.resolve('jquery')), 'utf8')
 );
 
 console.info('Finding the email address list edit button...');
 await page.evaluate(() => {
-	$('tr:contains("Service Emails Forwarding"')
+	console.log($('tr:contains("Service Emails Forwarding")')
 		.children('a:contains("Edit")')
-		.attr('id', 'edit-button');
+		.attr('id', 'edit-button'));
 });
 
+console.info('Pressing the edit button...');
 const serviceEmailsEditButton = await page.$('#edit-button');
+console.log(serviceEmailsEditButton);
 await serviceEmailsEditButton?.click();
