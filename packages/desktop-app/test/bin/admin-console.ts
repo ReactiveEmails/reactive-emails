0f8cdd9ef3d17aec6576d42834943a9d998476fb
Leon Si <contact@leonzalion.com>
2022-03-18 15:36:27 -0400

    feat: automation now works

--- packages/desktop-app/test/bin/admin-console.ts
+++ packages/desktop-app/test/bin/admin-console.ts
@@ -4,18 +4,19 @@ import * as path from 'node:path';
 import puppeteer from 'puppeteer';
 import $ from 'jquery';
 import pWaitFor from 'p-wait-for';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz');
 
 const require = createRequire(import.meta.url);
 
-const browser = await puppeteer.launch({ headless: false });
+fs.mkdirSync('context', { recursive: true });
+const browser = await puppeteer.launch({
+	headless: false,
+	userDataDir: 'context',
+});
 const page = await browser.newPage();
 await page.setBypassCSP(true);
-
-// Load cookies if available
-if (fs.existsSync('cookies.json')) {
-	const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8')) as any[];
-	await page.setCookie(...cookies);
-}
 
 console.info('Navigating to Sign in page...');
 await page.goto('https://accounts.google.com/signin/v2');
@@ -23,13 +24,8 @@ await page.goto('https://accounts.google.com/signin/v2');
 console.info('Waiting for myaccount.google.com...');
 await pWaitFor(() => page.url().startsWith('https://myaccount.google.com'));
 
-const cookies = await page.cookies();
-const cookiesJson = JSON.stringify(cookies);
-
-fs.writeFileSync('cookies.json', cookiesJson);
-
-console.info('Navigating to admin console...');
-await page.goto('https://admin.google.com/ac/apps/gmail/routing');
+console.info('Navigating to Address Lists in Admin Console...');
+await page.goto('https://admin.google.com/ac/apps/gmail/manageaddresslist');
 
 console.info('Waiting for admin.google.com...');
 await pWaitFor(() => page.url().startsWith('https://admin.google.com'));
@@ -39,14 +35,102 @@ await page.evaluate(
 	fs.readFileSync(path.join(require.resolve('jquery')), 'utf8')
 );
 
-console.info('Finding the email address list edit button...');
-await page.evaluate(() => {
-	console.log($('tr:contains("Service Emails Forwarding")')
-		.children('a:contains("Edit")')
-		.attr('id', 'edit-button'));
-});
+const clickInPage = async (elementId: string) => {
+	await page.evaluate((elementId: string) => {
+		// eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+		(document.querySelector(`#${elementId}`) as HTMLElement).click();
+	}, elementId);
+};
 
-console.info('Pressing the edit button...');
-const serviceEmailsEditButton = await page.$('#edit-button');
-console.log(serviceEmailsEditButton);
-await serviceEmailsEditButton?.click();
+{
+	console.info('Finding the email address list edit button...');
+	const elementId = nanoid();
+	await page.waitForFunction(
+		(elementId: string) =>
+			$('tr:contains("Service Emails")')
+				.find('a:contains("Edit")')
+				.attr('id', elementId).length > 0,
+		{},
+		elementId
+	);
+	await clickInPage(elementId);
+}
+
+{
+	console.info('Finding the bulk add addresses button...');
+	const elementId = nanoid();
+	await page.waitForFunction(
+		(elementId: string) =>
+			$('div[role="button"]')
+				.filter(function () {
+					return $(this).find('span:contains("Bulk-add addresses")').length > 0;
+				})
+				.eq(0)
+				.attr('id', elementId).length > 0,
+		{},
+		elementId
+	);
+	await clickInPage(elementId);
+}
+
+{
+	console.info('Finding the email address textbox...');
+	const elementId = nanoid();
+	await page.waitForFunction(
+		(elementId: string) =>
+			$('textarea[aria-label="Email address or domain name:"]').attr(
+				'id',
+				elementId
+			).length > 0,
+		{},
+		elementId
+	);
+
+	await page.evaluate((elementId: string) => {
+		// eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+		(document.querySelector(`#${elementId}`) as HTMLTextAreaElement).value =
+			'myemail@leonzalion.com';
+	}, elementId);
+}
+
+{
+	console.info('Finding the add addresses button...');
+	const elementId = nanoid();
+	await page.waitForFunction(
+		(elementId: string) =>
+			$('div[role="dialog"][aria-label*="Bulk-add addresses"]')
+				.eq(0)
+				.find('div[role="button"]')
+				.filter(function () {
+					console.log($(this).find('*'));
+					return $(this).find('span:contains("Add")').length > 0;
+				})
+				.eq(0)
+				.attr('id', elementId).length > 0,
+		{},
+		elementId
+	);
+	await clickInPage(elementId);
+}
+
+{
+	console.info('Pressing the Save button...');
+	const elementId = nanoid();
+	await page.waitForFunction(
+		(elementId: string) =>
+			$('div[role="dialog"][aria-label*="Edit address list"]')
+				.eq(0)
+				.find('div[role="button"]:contains("Save")')
+				.eq(0)
+				.attr('id', elementId).length > 0,
+		{},
+		elementId
+	);
+	await clickInPage(elementId);
+}
+
+await page.waitForFunction(
+	() => $('div:contains("Manage address lists settings updated"').length > 0
+);
+
+await browser.close();
