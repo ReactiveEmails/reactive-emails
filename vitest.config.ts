efd4a395a491a1769108a14c9b12716a09271e50
Leon Si <contact@leonzalion.com>
2022-04-13 00:42:36 -0400

    fix: lint issues

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { join } from 'desm';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			'~': join(import.meta.url, 'src')
+		}
+	}
+})
